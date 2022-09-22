131ddd7af0b191bd65ef5337399b45b03051b922
LeandroCaponetto <caponettopeppers@gmail.com>
2022-09-22 11:33:06 -0300

    porfolio

---
+++ main.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems,{
+      indicators: false,
+      interval: 4000
+    });
+
+
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+
+
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
